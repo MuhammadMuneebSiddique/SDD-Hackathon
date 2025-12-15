d8fb64942121cc644614c60cc5ebdd125319895e
MuhammadMuneebSiddiqui <siddiquimuneeb894@gmail.com>
2025-12-15 17:34:31 +0500

    API Integration

--- Physical_Humanoid_AI_Robotics/frontend/src/API/api.js
+++ Physical_Humanoid_AI_Robotics/frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://sdd-hackathon-production.up.railway.app/"
+    baseURL:"https://sdd-hackathon-production.up.railway.app"
 })
 
 export default  api
